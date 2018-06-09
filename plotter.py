e1607532a336103c32f1a1d577ea078c75586b9f
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2018-06-09 03:24:16 -0700

    Added zero function data

--- plotter.py
+++ plotter.py
@@ -19,16 +19,21 @@ predictions = []
 
 counts = 0
 sum_squared_error = 0
+zero_error = 0
 
 for i in range(len(true_array)):
     sum_squared_error = sum_squared_error + (true_array[i, search_example, 0] - predicted_array[i, search_example, 0]) ** 2
     sum_squared_error = sum_squared_error + (true_array[i, search_example, 1] - predicted_array[i, search_example, 1]) ** 2
+    zero_error = zero_error + (true_array[i, search_example, 0]) ** 2
+    zero_error = zero_error + (true_array[i, search_example, 1]) ** 2
     counts = counts + 2
     if i % 10 == 0 or i == range(len(true_array)):
         trues.append(true_array[i, search_example])
     predictions.append(predicted_array[i, search_example])
 
 print(sum_squared_error / counts)
+print(zero_error / counts)
+
 trues = np.asarray(trues)
 predictions = np.asarray(predictions)
 indices = range(len(trues))
