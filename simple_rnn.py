81f94ca577d2d9f72af3582397b156e18f0d74a8
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2018-06-08 19:59:01 -0700

    Added test loss on new tensorboard

--- simple_rnn.py
+++ simple_rnn.py
@@ -1,24 +1,28 @@
 import tensorflow as tf
 import preprocessor as p
 import numpy as np
+import sys
+
+RUN_VALUE = int(sys.argv[1])
 
 TRAINING_EPOCHS = 100000
-SAVING_EPOCHS = 15
+PLOTTING_EPOCHS = 10
+TEST_EPOCHS = 5
 
 LEARNING_RATE = 0.001
 
 BATCH_SIZE = 1024
-SEQUENCE_LENGTH = 400
-SKIP_LENGTH = 200
-HIDDEN_LAYER_SIZE = 200
+SEQUENCE_LENGTH = 500
+SKIP_LENGTH = 100
+HIDDEN_LAYER_SIZE = 50
 
 INPUT_SIZE = 192
 RNN_OUTPUT_SIZE = HIDDEN_LAYER_SIZE
-INTERMEDIATE_SIZE = 50
+INTERMEDIATE_SIZE = 30
 FINAL_OUTPUT_SIZE = 2
 
 
-datafiles = ['R_2016-01-27_P','R_2016-01-28_P','R_2016-01-29_P','R_2016-02-02_P', 'R_2017-11-02_P' ]
+datafiles = ['R_2016-01-27_P']  # , 'R_2016-01-28_P', 'R_2016-01-29_P', 'R_2016-02-02_P', 'R_2017-11-02_P']
 X = tf.placeholder(dtype=tf.float32, shape=[None, SEQUENCE_LENGTH, INPUT_SIZE])
 Y = tf.placeholder(dtype=tf.float32, shape=[None, SEQUENCE_LENGTH, FINAL_OUTPUT_SIZE])
 
@@ -35,18 +39,19 @@ predicted_out = simple_rnn(X)
 tf.shape(predicted_out)
 actual_Y = tf.transpose(Y, perm=[1, 0, 2])
 loss_op = tf.losses.mean_squared_error(labels=actual_Y, predictions=predicted_out)
-tf.summary.histogram('Actual Y', actual_Y)
-tf.summary.histogram('Predicted Y', predicted_out)
-tf.summary.histogram("Gradients", tf.gradients(loss_op, X))
 tf.summary.scalar('loss', loss_op)
+
 optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
 gvs = optimizer.compute_gradients(loss_op)
 capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
 train_op = optimizer.apply_gradients(capped_gvs)
 print("Done making graph")
 
-X_all, Y_all = p.preprocess(datafiles, "velocity", seq_length=SEQUENCE_LENGTH, skip_length = SKIP_LENGTH)
+X_all, Y_all = p.preprocess(datafiles, "position_relative", seq_length=SEQUENCE_LENGTH, skip_length=SKIP_LENGTH)
 data_split = p.set_split(X_all, Y_all, {"train": 0.8, "dev": 0.15, "test": 0.05})
+print(len(data_split["train"][0]))
+print(len(data_split["dev"][0]))
+print(len(data_split["test"][0]))
 dataset = p.Dataset(data_split["train"][0], data_split["train"][1])
 
 merged = tf.summary.merge_all()
@@ -54,7 +59,8 @@ merged = tf.summary.merge_all()
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
-    train_writer = tf.summary.FileWriter("tensorboard/run213", sess.graph)
+    train_writer = tf.summary.FileWriter("tensorboard/run" + str(RUN_VALUE), sess.graph)
+    test_writer = tf.summary.FileWriter("tensorboard/test_run" + str(RUN_VALUE), sess.graph)
 
     batch = 1
     for step in range(TRAINING_EPOCHS):
@@ -62,21 +68,28 @@ with tf.Session() as sess:
         while True:
             print("New batch")
             batch_x, batch_y = dataset.get_next_batch(BATCH_SIZE)
-            if batch_x is None:
-                print("Epoch: " + str(step) + " | Loss: " + str(loss))
-                break
+            # if batch_x is None:
+            #     print("Epoch: " + str(step) + " | Loss: " + str(loss))
+            #     break
             assert batch_x.shape == (BATCH_SIZE, SEQUENCE_LENGTH, INPUT_SIZE)
             assert batch_y.shape == (BATCH_SIZE, SEQUENCE_LENGTH, FINAL_OUTPUT_SIZE)
             sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
             summary, loss = sess.run([merged, loss_op], feed_dict={X: batch_x, Y: batch_y})
+
+            if step % PLOTTING_EPOCHS == 0:
+                print("Plotting values")
+                predictions, true = sess.run([predicted_out, actual_Y], feed_dict={X: batch_x, Y: batch_y})
+                np.save("output/run" + str(RUN_VALUE) + "/pred_epoch" + str(step), predictions)
+                np.save("output/run" + str(RUN_VALUE) + "/true_epoch" + str(step), true)
+
+            if step % TEST_EPOCHS == 0:
+                print("Calculating test loss")
+                test_loss = sess.run([loss_op], feed_dict={X: data_split["dev"][0], Y: data_split["dev"][1]})
+                test_summary = tf.Summary()
+                test_summary.value.add(tag='test_loss', simple_value=test_loss[0])
+                test_writer.add_summary(test_summary, batch)
+
             train_writer.add_summary(summary, batch)
             batch = batch + 1
             print("Epoch: " + str(step) + " | Loss: " + str(loss))
-
-            # TODO: Move into batch kill loop for normal implementation
-            #if step % SAVING_EPOCHS == 0:
-             #   print("Saving Epochs")
-              #  predictions, true = sess.run([predicted_out, actual_Y], feed_dict={X: batch_x, Y: batch_y})
-               # np.save("output/run22/pred_epoch" + str(step), predictions)
-               # np.save("output/run22/true_epoch" + str(step), true)
-            #break
+            break
